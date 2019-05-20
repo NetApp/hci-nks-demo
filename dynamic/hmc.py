d67d829020cfe2b34e42e00382fe482986932392
Matt White <whim@netapp.com>
2019-05-20 13:39:01 -0400

    Initial import

---
+++ dynamic/hmc.py
@@ -0,0 +1,26 @@
+from aiohttp import web  # Use the AIO Http Lib
+
+import os
+import aiohttp_jinja2
+import jinja2
+import datetime
+
+PRESENTER = os.environ.get('PRESENTER', "Unknown")
+ENV = os.environ.get('ENVIRONMENT', "unknown")
+
+
+# Handle a request mapped from the URL below and process it through the template
+@aiohttp_jinja2.template('./templates/index.jinja2')
+async def handle(request):
+    now = datetime.datetime.now()
+    return {"presenter": PRESENTER, "date": now.strftime("%Y%m%d%H%M%S"), "env": ENV}
+
+
+app = web.Application()  # Initialize the base web application
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(''))  # tell the template system where to look for templates
+
+app.router.add_get('/index.html', handle)  # Configure a URL
+app.router.add_get('/', handle)  # Configure a URL
+
+web.run_app(app)  # Loop and listen for connections
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Hybrid MultiCloud Demo
+
+1. Do some things to the code
+1. Run the code locally on Docker
+1. Commit the code to github
+1. Watch it build on docker.io
+1. Deploy it to K8s in one cloud
+1. Deploy it to K8s in another cloud
+
